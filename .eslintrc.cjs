07b4160a7c49d675c7f6d8c3d507580e22d4e034
Stefan Schwandter <s.schwandter@me.com>
2023-10-22 15:39:16 +0200

    Eslint config for jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,17 @@ module.exports = {
     browser: false,
     es2021: true,
   },
-  extends: ["eslint:recommended", "node:recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:node/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: "2023",
     sourceType: "module",
   },
+  overrides: [
+    {
+      files: ["*.test.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+    },
+  ],
 };
